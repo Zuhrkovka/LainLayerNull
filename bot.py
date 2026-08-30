76abd2a2a7ef1db3f9678b17f59cf9fd4fbb6399
Mia <MiaWiens@Tuta.io>
2026-08-30 17:19:47 +0200

    Bot first steps

---
+++ bot.py
@@ -0,0 +1,24 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Everything is connected!")
+
+
+bot.run(os.getenv("DISCORD_TOKEN"))
